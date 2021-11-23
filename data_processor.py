1fecb4b17b8715929f8ec3677778a2946a44b7d6
alam51 <jccbuet1@gmail.com>
2021-11-23 18:16:00 +0600

    commit 1

--- data_processor.py
+++ data_processor.py
@@ -11,7 +11,6 @@ class DfProcessor:
         2. 'Sp' for SCADA (before last update)
         3. to be updated....
     """
-
     def __init__(self, csv_file_path: str, source='S'):
         raw_df = pd.read_csv(csv_file_path, skiprows=[1], parse_dates=[0],
                              infer_datetime_format=True, index_col=[0], dayfirst=False)
@@ -21,24 +20,25 @@ class DfProcessor:
 
 
 # file_path = 'F:/Summit Bib 2021.11.16.csv'
-file_path = 'F:/Bhola_Notun_Biddyut/2021.11.09.csv'
-df = DfProcessor(file_path)
-
+# file_path = 'F:/Bhola_Notun_Biddyut/2021.11.09.csv'
+# df = DfProcessor(file_path)
+#
 # df.df.plot(secondary_y=[df.df.columns[1]])
-x_data = df.df.iloc[:, 1].to_list()
-y_data = df.df.iloc[:, 0].to_list()
-# df.df.plot.scatter(x=df.df.columns[1], y=df.df.columns[0])
-slope, intercept, r, p, std_err = stats.linregress(x_data, y_data)
-
-
-def myfunc(x):
-    return slope * x + intercept
-
-
-# df.df.loc['2021-11-08 18'].plot.scatter(x=df.df.columns[1], y=df.df.columns[0])
-mymodel = list(map(myfunc, x_data))
-plt.scatter(x_data, y_data)
-plt.plot(x_data, mymodel, color='r', label=f'slope={slope},\nintercept={intercept}')
-plt.legend()
-plt.show()
-b = 4
+# plt.show()
+# x_data = df.df.loc['2021-11-09 01:00':'2021-11-09 19:00'].iloc[:, 1].to_list()
+# y_data = df.df.loc['2021-11-09 01:00':'2021-11-09 19:00'].iloc[:, 0].to_list()
+# # df.df.plot.scatter(x=df.df.columns[1], y=df.df.columns[0])
+# slope, intercept, r, p, std_err = stats.linregress(x_data, y_data)
+# droop = (intercept-50.0)/50.0
+#
+# def myfunc(x):
+#     return slope * x + intercept
+#
+#
+# # df.df.loc['2021-11-08 18'].plot.scatter(x=df.df.columns[1], y=df.df.columns[0])
+# mymodel = list(map(myfunc, x_data))
+# plt.scatter(x_data, y_data)
+# plt.plot(x_data, mymodel, color='r', label=f'slope={slope}\nintercept={intercept}\ndroop={droop*100}%')
+# plt.legend()
+# plt.show()
+# b = 4
--- droop_analyzer.py
+++ droop_analyzer.py
@@ -3,13 +3,17 @@ from scipy import stats
 
 from data_processor import DfProcessor
 
-file_path = 'F:/Bhola_Notun_Biddyut/2021.11.09.csv'
+# file_path = 'F:/Bhola_Notun_Biddyut/2021.11.09.csv'
+file_path = 'F:/Summit Bib 2021.11.16.csv'
 df = DfProcessor(file_path)
 # df.df.plot(secondary_y=[df.df.columns[1]])
-x_data = df.df.iloc[:, 1].to_list()
-y_data = df.df.iloc[:, 0].to_list()
+x_data = df.df.loc[:].iloc[:, 1].to_list()
+# x_data = df.df.loc['2021-11-09 01:00':'2021-11-09 19:00'].iloc[:, 1].to_list()
+y_data = df.df.loc[:].iloc[:, 0].to_list()
+# y_data = df.df.loc['2021-11-09 01:00':'2021-11-09 19:00'].iloc[:, 0].to_list()
 # df.df.plot.scatter(x=df.df.columns[1], y=df.df.columns[0])
 slope, intercept, r, p, std_err = stats.linregress(x_data, y_data)
+droop = (intercept-50.0)/50.0
 
 
 def myfunc(x):
@@ -19,7 +23,15 @@ def myfunc(x):
 # df.df.loc['2021-11-08 18'].plot.scatter(x=df.df.columns[1], y=df.df.columns[0])
 mymodel = list(map(myfunc, x_data))
 plt.scatter(x_data, y_data)
-plt.plot(x_data, mymodel, color='r', label=f'slope={slope},\nintercept={intercept}')
-plt.legend()  # must be added to show custom label
-# plt.show()  # no more needed as plt.legend() added
+plt.xlabel('Active Power [MW]', fontsize=15)
+plt.ylabel('Frequency [Hz]', fontsize=15)
+plt.plot(x_data, mymodel, color='r', label=f'slope={slope:.4f}Hz/MW\n'
+                                           f'gain={1/slope:.2f}MW/Hz\n'
+                                           f'intercept={intercept:.2f}Hz\n'
+                                           f'droop={droop*100:.2f}%\n'
+                                           f'error={std_err*100:.4f}%'
+         )
+plt.grid(True)
+plt.legend(fontsize=16)
+plt.show()
 b = 4
